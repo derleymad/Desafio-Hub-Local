4982ca7b11941b543ac8aa79f9121bcdcff3f907
derleymad <filho.wanderley@hotmail.com>
2022-01-18 22:49:17 -0300

    Criando pastas em src e definindo appconfig.ts em utils

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  site_name: 'Desafio Hub Local',
+  title: 'Desafio - HubLocal ',
+  description: 'Solução feita para concorrer ao Programa de Bolsas HubDev',
+  locale: 'pt',
+};
