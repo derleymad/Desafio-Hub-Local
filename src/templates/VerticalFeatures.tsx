dbe3b1de725bc49935fddd1c6bca8c9fd1d789bc
derleymad <70535956+derleymad@users.noreply.github.com>
2022-01-21 12:33:39 -0300

    Atualizando alt das imagens

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -10,26 +10,26 @@ const VerticalFeatures = () => (
       title="Analisando presença..."
       description="Saiba se a sua empresa está de forma otimizada e presente em todos os mapas e listas da internet."
       image="/assets/images/feature.svg"
-      imageAlt="First feature alt text"
+      imageAlt="Ilustração de um homem interagindo com localidade"
     />
     <VerticalFeatureRow
       title="Atualizando informações..."
       description="Mantenha as informações sobre o seu negócio sempre atualizadas e completas e não perca tempo para a sua concorrência."
       image="/assets/images/feature2.svg"
-      imageAlt="Second feature alt text"
+      imageAlt="Ilustração de uma mulher interagindo com um texto atualizado"
       reverse
     />
     <VerticalFeatureRow
       title="Divulgando o seu negócio..."
       description="A HubLocal divulga facilmente promoções, cupons e campanhas nos site e apps que mais interessam."
       image="/assets/images/feature3.svg"
-      imageAlt="Third feature alt text"
+      imageAlt="Ilustração de duas mulheres perto de um símbolo share"
     />
     <VerticalFeatureRow
       title="Procurando seus clientes..."
       description="Com os relatórios da HubLocal, receba informações vitais para aumentar seus resultados e ter novos clientes. "
       image="/assets/images/feature4.svg"
-      imageAlt="Second feature alt text"
+      imageAlt="Ilustração de uma mulher com um mapa no smartphone"
       reverse
     />
   </Section>
